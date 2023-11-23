be928669ba1a03466f83aa5e88edbb292868305d
tyranoboy1 <gkwlgnd@naver.com>
2023-11-24 00:33:37 +0900

    test commit

--- src/context/AuthContext.tsx
+++ src/context/AuthContext.tsx
@@ -4,10 +4,12 @@ import { createContext, useState } from "react";
 import { IAuthContextProps } from "./interface/authContext.interface";
 import React from "react";
 
+/** Context 객체 생성 */
 const AuthContext = createContext({
   user: null as User | null,
 });
 
+/** Context Provider 생성 */
 export const AuthContextProvider = ({ children }: IAuthContextProps) => {
   const auth = getAuth(app);
   const [currentUser, setCurrentUser] = useState<User | null>(null);
