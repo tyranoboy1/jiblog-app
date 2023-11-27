52ed903fd121795c0781d672081234445180f4de
tyranoboy1 <gkwlgnd@naver.com>
2023-11-28 00:37:34 +0900

    font setting

--- src/context/ModalContext.tsx
+++ src/context/ModalContext.tsx
@@ -4,6 +4,7 @@ import {
   IModalContextProps,
 } from "context/interface/context.interface";
 
+/** 모달 Context 객체 생성 */
 const ModalContext = createContext<IModalContextData>({
   modalMessage: null,
   isModalShow: false,
@@ -12,18 +13,22 @@ const ModalContext = createContext<IModalContextData>({
   onConfirm: () => {},
 });
 
+/** ModalContext를 사용하기 위한 커스텀 훅  => 커스텀 훅을 통해 모달 상태 접근 */
 export const useModal = () => useContext(ModalContext);
 
 export const ModalContextProvider = ({ children }: IModalContextProps) => {
+  /** 모달 메세지, 표시 여부, 확인 콜백 함수에 대한 상태 관리 */
   const [modalMessage, setModalMessage] = useState<string | null>(null);
   const [isModalShow, setIsModalShow] = useState<boolean>(false);
   const [onConfirm, setOnConfirm] = useState<() => void>(() => {});
 
+  /** 모달을 보여주는 함수 */
   const showModal = (message: string, onConfirmAction: () => void) => {
     setModalMessage(message);
     setIsModalShow(true);
     setOnConfirm(() => onConfirmAction);
   };
+  /** 모달을 숨기는 함수 */
   const hideModal = () => {
     setModalMessage(null);
     setIsModalShow(false);
